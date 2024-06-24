8935abd4f308553365c6f5ba6f04657a05140fa7
wilderif <woojuro3@gmail.com>
2024-06-24 20:32:48 +0900

    fix: Adjust sidebar layout to fix button positions and scrolling - Modified sidebar layout to use flex-grow for positioning elements. - Ensured the ’Setting’ button is fixed at the bottom of the sidebar. - Adjusted the ’+ New Space’ button to remain visible while allowing the space list to scroll.

--- src/components/sidebar/SpaceSidebar.jsx
+++ src/components/sidebar/SpaceSidebar.jsx
@@ -29,17 +29,17 @@ const SpaceSidebar = ({
         />
       )}
 
-      <aside className="w-52 rounded-r-lg bg-stone-900 pl-6 pr-6 pt-12 font-sans text-stone-50">
+      <aside className="flex h-screen w-48 min-w-48 max-w-48 flex-col rounded-r-lg bg-stone-900 pl-6 pr-6 pt-12 font-sans text-stone-50">
         <h2 className="mb-10 font-bold uppercase text-stone-200">
           Temp header
         </h2>
 
-        <div className="">
-          <div>
+        <div className="flex flex-grow flex-col overflow-hidden">
+          <div className="overflow-y-scroll">
             <ul>
               {spaces.map((space) => {
                 let cssClasses =
-                  "mb-2 w-full rounded-sm px-2 py-1 text-left text-sm";
+                  "mb-2 w-full rounded-sm px-2 py-1 text-left text-sm overflow-hidden truncate ";
 
                 if (space.id === currentSpaceId) {
                   cssClasses += " bg-stone-600 text-stone-200";
@@ -65,18 +65,16 @@ const SpaceSidebar = ({
           </div>
 
           <button
-            className="w-full rounded-md px-2 py-1 text-left font-sans text-sm text-stone-400 hover:bg-stone-600 hover:text-stone-100"
+            className=" w-full rounded-md px-2 py-1 text-left font-sans text-sm text-stone-400 hover:bg-stone-600 hover:text-stone-100"
             onClick={handleOnClickAddSpace}
           >
             + New Space
           </button>
+        </div>
 
-          <div className="mt-[420px]">
-            <div className="mb-2 w-full border-[1px] border-stone-500"></div>
-            <div>
-              <SidebarConfigButton>Setting</SidebarConfigButton>
-            </div>
-          </div>
+        <div className="my-2">
+          <div className="mb-2 w-full border-[1px] border-stone-500"></div>
+          <SidebarConfigButton>Setting</SidebarConfigButton>
         </div>
       </aside>
     </>
